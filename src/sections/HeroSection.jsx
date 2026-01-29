a01f1dbcb7efd5b5e7eb332508ccb23a2f2fdacc
Adidata - Moh. Iqbal Firman Ardiansyah <adidata.ardiansyah@bankmandiri.co.id>
2026-01-29 21:06:45 +0700

    edit: cv

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -11,13 +11,13 @@ const HeroSection = () => {
                     {/* <p className="text-6xl font-semibold">disini</p> */}
                 </div>
                 <div className="flex items-center mt-8 gap-8">
-                    <a href="/" className="flex gap-2 px-4 md:px-8 py-2 md:py-4 bg-blue-500 rounded-lg text-white text-base font-semibold hover:bg-yellow-500 shadow-xl transition-colors">
+                    <a href="https://github.com/iqbalf-a" className="flex gap-2 px-4 md:px-8 py-2 md:py-4 bg-blue-500 rounded-lg text-white text-base font-semibold hover:bg-yellow-500 shadow-xl transition-colors">
                         <div className="flex items-center gap-2">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
                             </svg>
-                            {/* <p className='text-sm md:text-base'>Download CV</p> */}
-                            <a href="https://github.com/iqbalf-a">github</a>
+                            <p className='text-sm md:text-base'>github CV</p>
+                            
                         </div>
                     </a>
                     {/* <a href="/" className="text-base underline">See Portfolio</a> */}
